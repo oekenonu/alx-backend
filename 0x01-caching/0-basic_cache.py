7dfbb3faba647abe1f3677b2470526486ee32d26
oekenonu <146424918+oekenonu@users.noreply.github.com>
2024-08-21 20:08:05 +0100

    Create 0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+"""Module to implement basic cache system"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """Creates a basic cache with no limit """
+
+    def put(self, key, item):
+        """add an item to cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieves item from cache"""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
